9184e592069631d3476b135b68379864a4fd72f5
a64650 <72816311+a64650@users.noreply.github.com>
2021-06-10 19:25:40 +0100

    code optimization

--- ceasers/ceaser.py
+++ ceasers/ceaser.py
@@ -2,15 +2,17 @@ def ceaseren(StringInput, key):
     result = ""
     for char in StringInput:
         if char.isupper():
-            result += chr((ord(char) + (key-65)) % 26 + 65)
-        else:
+            result += chr((ord(char) + (key - 65)) % 26 + 65)
+        elif char.islower():
             result += chr((ord(char) + (key - 97)) % 26 + 97)
+        else:
+            result += char
     return result
 
 
 def ceaserdec(StringInput, key):
     key = int(key) % 26
-    return ceaseren(StringInput, 26-key)
+    return ceaseren(StringInput, 26 - key)
 
 
 print(ceaseren("abc", 1))
